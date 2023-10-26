2bc7cfbef2dbc77711f0c38e7ff3d00bda5c9866
muhammadrezamaulana123 <mrezamaulana9346@gmail.com>
2023-10-26 00:53:28 -0700

    ini commit

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,11 +14,24 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)->create();
+        User::factory(20)->create();
         User::create([
-            'name' => 'Muhammad Reza Maulana',
-            'email' => 'mrezamaulana9346@gmail.com',
+            'name' => 'Saiful Bahri',
+            'email' => 'bahri@gmail.com',
             'email_verified_at'  => now(),
+            'role' => 'admin',
+            'phone' => '6285640899224',
+            'bio' => 'flutter dev',
+            'password' => Hash::make('123456'),
+        ]);
+
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'email_verified_at'  => now(),
+            'role' => 'superadmin',
+            'phone' => '6285640899225',
+            'bio' => 'laravel dev',
             'password' => Hash::make('123456'),
         ]);
     }
